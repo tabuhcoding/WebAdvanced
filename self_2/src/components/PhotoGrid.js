0631b1c573a255fd253e6304158868e391b4a8a1
tabuhcoding <134000982+tabuhcoding@users.noreply.github.com>
2024-10-16 16:58:08 +0700

    fix

--- self_2/src/components/PhotoGrid.js
+++ self_2/src/components/PhotoGrid.js
@@ -1,47 +1,36 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../style/PhotoGrid.css';
 
-const PhotoGrid = ({ photos, lastPhotoElementRef }) => {
+const PhotoGrid = ({ photos }) => {
+  const navigate = useNavigate();
+
+  const handlePhotoClick = (photo) => {
+    navigate(`/photos/${photo.id}`);
+  };
+
   return (
     <div className="container mt-4">
       <div className="row">
-        {photos.map((photo, index) => {
-          // If this is the last photo, attach the ref to it
-          if (photos.length === index + 1) {
-            return (
-              <div
-                key={photo.id}
-                className="col-6 col-md-4 col-lg-3 mb-4"
-                ref={lastPhotoElementRef}
-              >
-                <div className="card">
-                  <img
-                    src={photo.urls.thumb}
-                    className="card-img-top"
-                    alt={photo.alt_description}
-                  />
-                  <div className="card-body">
-                    <p className="card-text text-center">{photo.user.name}</p>
-                  </div>
-                </div>
+        {photos.map((photo) => (
+          <div
+            key={photo.id}
+            className="col-6 col-md-4 col-lg-3 mb-4"
+            onClick={() => handlePhotoClick(photo)}
+            style={{ cursor: 'pointer' }}
+          >
+            <div className="card">
+              <img
+                src={photo.urls.thumb}
+                className="card-img-top"
+                alt={photo.alt_description}
+              />
+              <div className="card-body">
+                <p className="card-text text-center">{photo.user.name}</p>
               </div>
-            );
-          } else {
-            return (
-              <div key={photo.id} className="col-6 col-md-4 col-lg-3 mb-4">
-                <div className="card">
-                  <img
-                    src={photo.urls.thumb}
-                    className="card-img-top"
-                    alt={photo.alt_description}
-                  />
-                  <div className="card-body">
-                    <p className="card-text text-center">{photo.user.name}</p>
-                  </div>
-                </div>
-              </div>
-            );
-          }
-        })}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
